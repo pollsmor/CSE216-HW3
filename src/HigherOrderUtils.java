b697c8212c0be02a2383891784a7f600d08f1aed
Kevin Li <kevinli4321@gmail.com>
2021-04-19 16:31:50 -0400

    Starter code

--- src/HigherOrderUtils.java
+++ src/HigherOrderUtils.java
@@ -1,3 +1,36 @@
+import java.util.List;
+import java.util.function.BiFunction;
+
 public class HigherOrderUtils {
-    
+    public static interface NamedBiFunction<T, U, R> extends BiFunction<T, U, R> {
+        String name();
+    }
+
+    /**
+     * Applies a given list of bifunctions -- functions that take two arguments of a certain type
+     * and produce a single instance of that type -- to a list of arguments of that type. The
+     * functions are applied in an iterative manner, and the result of each function is stored in
+     * the list in an iterative manner as well, to be used by the next bifunction in the next
+     * iteration. For example, given
+     *      List<Double> args = Arrays.asList(1d, 1d, 3d, 0d, 4d), and
+     *      List<NamedBiFunction<Double, Double, Double>> bfs = [add, multiply, add, divide],
+     * <code>zip(args, bfs)</code> will proceed iteratively as follows:
+     *      - index 0: the result of add(1,1) is stored in args[1] to yield args = [1,2,3,0,4]
+     *      - index 1: the result of multiply(2,3) is stored in args[2] to yield args = [1,2,6,0,4]
+     *      - index 2: the result of add(6,0) is stored in args[3] to yield args = [1,2,6,6,4]
+     *      - index 3: the result of divide(6,4) is stored in args[4] to yield args = [1,2,6,6,1]
+     *
+     * @param args:         the arguments over which <code>bifunctions</code> will be applied.
+     * @param bifunctions:  the list of bifunctions that will be applied on <code>args</code>.
+     * @param <T>:          the type parameter of the arguments (e.g., Integer, Double)
+     * @return              the item in the last index of <code>args</code>, which has the final
+     *                      result of all the bifunctions being applied in sequence.
+     */
+    public static <T> T zip(List<T> args, List<NamedBiFunction<T, T, T>> bifunctions) {
+        return null;
+    }
+
+    public static class FunctionComposition<T, U, R> {
+
+    }
 }
--- src/StreamUtils.java
+++ src/StreamUtils.java
@@ -1,3 +1,57 @@
-public class StreamUtils {
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
+public class StreamUtils {
+    /**
+     * @param strings:  the input collection of <code>String</code>s.
+     * @return          a collection of those <code>String</code>s in the input collection
+                        that start with a capital letter.
+     */
+    public static Collection<String> capitalized(Collection<String> strings) {
+        return strings;
+    }
+
+    /**
+     * Find and return the longest <code>String</code> in a given collection of <code>String</code>s.
+     *
+     * @param strings:      the given collection of <code>String</code>s.
+     * @param from_start:   a <code>boolean</code> flag that decides how ties are broken.
+                            If <code>true</code>, then the element encountered earlier in
+     *                      the iteration is returned, otherwise the later element is returned.
+     * @return              the longest <code>String</code> in the given collection,
+     *                      where ties are broken based on <code>from_start</code>.
+     */
+    public static String longest(Collection<String> strings, boolean from_start) {
+        return "";
+    }
+
+    /**
+     * Find and return the least element from a collection of given elements that are comparable.
+     *
+     * @param items:        the given collection of elements
+     * @param from_start:   a <code>boolean</code> flag that decides how ties are broken.
+     *                      If <code>true</code>, the element encountered earlier in the
+     *                      iteration is returned, otherwise the later element is returned.
+     * @param <T>:          the type parameter of the collection (i.e., the items are all of type T).
+     * @return              the least element in <code>items</code>, where ties are
+     *                      broken based on <code>from_start</code>.
+     */
+    public static <T extends Comparable<T>> T least(Collection<T> items, boolean from_start) {
+        return null;
+    }
+
+    /**
+     * Flattens a map to a stream of <code>String</code>s, where each element in the list
+     * is formatted as "key -> value" (i.e., each key-value pair is converted to a string
+     * with this format).
+     *
+     * @param aMap: the specified input map.
+     * @param <K>:  the type parameter of keys in <code>aMap</code>.
+     * @param <V>:  the type parameter of values in <code>aMap</code>.
+     * @return      the flattened list representation of <code>aMap</code>.
+     */
+    public static <K, V> List<String> flatten(Map<K, V> aMap) {
+        return null;
+    }
 }
